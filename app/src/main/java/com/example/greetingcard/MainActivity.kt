df57bc972fe1176a650f5ac9c8b7f1cca999b37b
CheoR <cheo.roman+github@gmail.com>
2023-06-06 08:03:25 -0500

    build: Ernesto greeting

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 
@@ -22,8 +32,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             GreetingCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
+                        from= stringResource(R.string.from_text),
+                        message = stringResource(R.string.message_text),
+                    )
                 }
             }
         }
@@ -32,11 +48,49 @@ class MainActivity : ComponentActivity() {
 
 // @Composable fun dont' return anything
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Blue) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier // .padding(8.dp),
+    ) {
         Text(
-            text = "$name is a little piggy!",
-            modifier = modifier.padding(24.dp)
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+//                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+
+}
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+
+    val image = painterResource(R.drawable.holabud)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+        )
+
+        GreetingText(
+            message=message,
+            from=from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+            ,
         )
     }
 }
@@ -47,6 +101,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("CheoR")
+        GreetingImage(
+            from=stringResource(R.string.from_text),
+            message = stringResource(R.string.message_text),
+        )
     }
 }
